bd0ef1a0ad8474727ccb19a0920babd2e5620216
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2024-08-23 17:07:55 +0500

    Added SEO codes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,76 @@ import "./i18n.jsx";
 import { Providers } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-    title: 'Usat Admission',
+    title: 'University of Science and Technologies',
     description:
-        "USAT muvaffaqiyatli kelajak shu yerdan boshlanadi.USAT - Fan va Texnologiyalar Universiteti o'z ichiga zamonaviy va samarali jahon standartlari asosidagi ta'limni qamrab olgan.",
+        'Learn about the admission process at the University of Science and Technology. Explore our programs, apply now, and start your journey towards a successful future.',
+    keywords:
+        'university admissions, science and technology, apply now, higher education, study programs, student applications',
+    robots: 'index, follow',
+    viewport: 'width=device-width, initial-scale=1.0',
+    authors: [
+        {
+            name: 'University of Science and Technology',
+            url: 'https://qabul.usat.uz/',
+        },
+    ],
+    icons: {
+        icon: [
+            { rel: 'icon', url: './favicon.ico', type: 'image/x-icon' },
+            {
+                rel: 'icon',
+                url: '/favicon-32x32.png',
+                sizes: '32x32',
+                type: 'image/png',
+            },
+            {
+                rel: 'icon',
+                url: '/favicon-16x16.png',
+                sizes: '16x16',
+                type: 'image/png',
+            },
+        ],
+        apple: [
+            {
+                rel: 'apple-touch-icon',
+                url: '/apple-touch-icon.png',
+                sizes: '180x180',
+            },
+        ],
+        
+    },
+    openGraph: {
+        title: 'University of Science and Technologies',
+        description:
+            'Learn about the admission process at the University of Science and Technology. Explore our programs, apply now, and start your journey towards a successful future.',
+        url: 'https://qabul.usat.uz/',
+        siteName: 'University of Science and Technologies',
+        images: [
+            {
+                url: 'https://qabul.usat.uz/assets/logos/back.png',
+                width: 1200,
+                height: 630,
+                alt: 'University of Science and Technologies',
+            },
+        ],
+        type: 'website',
+    },
+    twitter: {
+        title: 'University of Science and Technologies',
+        card: 'summary_large_image',
+        description:
+            'Learn about the admission process at the University of Science and Technology. Explore our programs, apply now, and start your journey towards a successful future.',
+        images: [
+            {
+                url: 'https://qabul.usat.uz/assets/logos/back.png',
+                width: 1200,
+                height: 630,
+                alt: 'University of Science and Technologies',
+            },
+        ],
+        creator: '@qabul.uz',
+    }
+    
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,46 @@ import Magistr from "@/customComponents/Magistr/App";
 import Questions from "@/customComponents/Questions/App";
  function Home() {
   return (
-    <main>
-       <Head>
-        <link rel="icon" href="/favicon.ico"></link>
-        </Head>
-        <Hero></Hero>
-        <About></About>
-        <Info></Info>
-        <Bachelor></Bachelor>
-        <MagistrInfo></MagistrInfo>
-        <Magistr></Magistr>
-        <Questions></Questions>
-    </main>
+      <main>
+          <Head>
+              <script type="application/ld+json">
+                  {JSON.stringify({
+                      '@context': 'https://schema.org',
+                      '@type': 'Organization',
+                      name: 'University of Science and Technology',
+                      url: 'https://qabul.usat.uz/',
+                      logo: 'https://qabul.usat.uz/assets/logos/usatLogo.svg',
+                      sameAs: [
+                          'https://www.facebook.com/usat.uz',
+                          'https://www.twitter.com/usat.uz',
+                          'https://www.linkedin.com/company/usat.uz',
+                      ],
+                      contactPoint: {
+                          '@type': 'ContactPoint',
+                          telephone: '+998-78-88-3888',
+                          contactType: 'Admissions',
+                          areaServed: 'UZ',
+                          availableLanguage: ['Uzbek', 'Russian', 'English'],
+                      },
+                      address: {
+                          '@type': 'PostalAddress',
+                          streetAddress: '123 Main St',
+                          addressLocality: 'Tashkent',
+                          addressRegion: 'Tashkent Region',
+                          postalCode: '100000',
+                          addressCountry: 'UZ',
+                      },
+                  })}
+              </script>
+          </Head>
+          <Hero></Hero>
+          <About></About>
+          <Info></Info>
+          <Bachelor></Bachelor>
+          <MagistrInfo></MagistrInfo>
+          <Magistr></Magistr>
+          <Questions></Questions>
+      </main>
   );
 }
 export default Home;
